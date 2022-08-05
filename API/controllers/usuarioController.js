0fa615f51307f29f56c284183135e2fd0a7f8193
Felipe Freire <felipef12123@gmail.com>
2022-08-05 10:03:04 -0300

    crud usuarios completo

--- API/controllers/usuarioController.js
+++ API/controllers/usuarioController.js
@@ -7,6 +7,7 @@ class usuarioController {
             return res.status(200).json(todosOsUsuarios)
         }
         catch (error) {
+            console.log("ERRO AO BUSCAR TODOS OS USUÁRIOS")
             return res.status(500).json(error.message)
         }
     }
@@ -21,6 +22,7 @@ class usuarioController {
             return res.status(200).json(umUsuario)    
         }
         catch(error) {
+            console.log("ERRO AO TENTAR BUSCAR UM USUÁRIO")
             return res.status(500).json(error.message)
         }
     }
@@ -32,7 +34,32 @@ class usuarioController {
             return res.status(200).json(novoUsuarioCriado)
         }
         catch(error) {
-            console.log("ERRO")
+            console.log("ERRO AO CRIAR NOVO USUÁRIO")
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async atualizaUsuario(req, res) {
+        const { id } = req.params
+        const novasInfos = req.body
+        try {
+            await database.Usuarios.update(novasInfos, { where: { id: Number(id) } } )
+            const usuarioAtualizado = await database.Usuarios.findOne({ where: { id: Number(id) } } )
+            return res.status(200).json(usuarioAtualizado)
+        }
+        catch(error) {
+            console.log("ERRO AO ATUALIZAR USUÁRIO")
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async removeUsuario(req, res) {
+        const { id } = req.params
+        try {
+            await database.Usuarios.destroy({ where: { id: Number(id) } } )
+            return res.status(200).json({ mensagem: `ID ${id} deletado.` })
+        }
+        catch(error) {
             return res.status(500).json(error.message)
         }
     }
--- API/routes/usuarioRoutes.js
+++ API/routes/usuarioRoutes.js
@@ -5,6 +5,8 @@ const router = Router()
 
 router.get('/usuarios', usuarioController.pegaTodosOsUsuarios)
 router.get('/usuarios/:id', usuarioController.pegaUmUsuario)
-router.post('usuarios', usuarioController.criaUsuario)
+router.post('/usuarios', usuarioController.criaUsuario)
+router.put('/usuarios/:id', usuarioController.atualizaUsuario)
+router.delete('/usuarios/:id', usuarioController.removeUsuario)
 
 module.exports = router
